c9b083f51d6954d6233b5019173e67a0edfedb16
yerim <1000gaji@gmail.com>
2024-02-27 22:17:42 +0900

    tiny 10

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -99,7 +99,7 @@ void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longms
     sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
     sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
 
-    sprintf(buf, "HTTP/1.0  %s %s\r\n", errnum, shortmsg);
+    sprintf(buf, "HTTP/1.1  %s %s\r\n", errnum, shortmsg);
     Rio_writen(fd, buf, strlen(buf));
     sprintf(buf, "Content-type: text/html\r\n");
     Rio_writen(fd, buf, strlen(buf));
@@ -154,7 +154,7 @@ void serve_static(int fd, char *filename, int filesize)
     char *srcp, filetype[MAXLINE], buf[MAXBUF];
 
     get_filetype(filename, filetype);
-    sprintf(buf, "HTTP/1.0 200 OK\r\n");
+    sprintf(buf, "HTTP/1.1 200 OK\r\n");
     sprintf(buf, "%sServer: Tiny Web Server\r\n", buf);
     sprintf(buf, "%sConnection: close\r\n", buf);
     sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
@@ -173,13 +173,14 @@ void serve_static(int fd, char *filename, int filesize)
 void serve_dynamic(int fd, char *filename, char *cgiargs)
 {
     char buf[MAXLINE], *emptylist[] = { NULL };
+    pid_t pid;
 
-    sprintf(buf, "HTTP/1.0 200 OK\r\n");
+    sprintf(buf, "HTTP/1.1 200 OK\r\n");
     Rio_writen(fd, buf, strlen(buf));
     sprintf(buf, "Server: Tiny Web Server\r\n");
     Rio_writen(fd, buf, strlen(buf));
 
-    if(Fork() == 0){
+    if((pid=Fork()) == 0){
         setenv("QUERY_STRING", cgiargs, 1); //setservent?
         Dup2(fd, STDOUT_FILENO);
         Execve(filename, emptylist, environ);
