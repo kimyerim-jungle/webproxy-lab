123da0145f00c146851fbd7a4beac3f8c5012794
yerim <1000gaji@gmail.com>
2024-02-27 17:11:17 +0900

    mpg->mp4로 대신 출력

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -9,8 +9,8 @@
 #include "csapp.h"
 
 void doit(int fd);
-//void read_requesthdrs(rio_t *rp);
-void read_requesthdrs(rio_t *rp, int fd, int ptr);
+void read_requesthdrs(rio_t *rp);
+//void read_requesthdrs(rio_t *rp, int fd, int ptr);
 int parse_uri(char *uri, char *filename, char *cgiargs);
 void serve_static(int fd, char *filename, int filesize);
 void get_filetype(char *filename, char *filetype);
@@ -53,22 +53,18 @@ void doit(int fd)
     char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
     char filename[MAXLINE], cgiargs[MAXLINE];
     rio_t rio;
-    int ptr;
-
-    ptr = Open("header.txt", O_RDWR, 0);
-
+    
     Rio_readinitb(&rio, fd);
     Rio_readlineb(&rio, buf, MAXLINE);
     printf("Request headers:\n");
     printf("%s", buf);
-    Write(ptr, buf, strlen(buf));
     sscanf(buf, "%s %s %s", method, uri, version);
     if (strcasecmp(method, "GET"))
     {
         clienterror(fd, method, "501", "Not implemented", "Tiny does not implement this method");
         return;
     }
-    read_requesthdrs(&rio, fd, ptr);
+    read_requesthdrs(&rio);
 
     is_static = parse_uri(uri, filename, cgiargs);
     if (stat(filename, &sbuf) < 0)
@@ -115,35 +111,34 @@ void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longms
 
 
 
-void read_requesthdrs(rio_t *rp, int fd, int ptr)
+void read_requesthdrs(rio_t *rp)
 {
-    char *srcp, buf[MAXLINE], header[MAXBUF];
-    struct stat sbuf;
+    char buf[MAXLINE];
 
     Rio_readlineb(rp, buf, MAXLINE);
 
     while (strcmp(buf, "\r\n")) {
         Rio_readlineb(rp, buf, MAXLINE);
         printf("%s", buf);
-        Write(ptr, buf, strlen(buf)); // 받아다 파일에 쓰기
+        // Write(ptr, buf, strlen(buf)); // 받아다 파일에 쓰기
     }
 
-    if (stat("header.txt", &sbuf) < 0)
-    {
-        clienterror(fd, "header.txt", "404", "Not found", "Tiny couldn't find this file");
-        return;
-    }
+    // if (stat("header.txt", &sbuf) < 0)
+    // {
+    //     clienterror(fd, "header.txt", "404", "Not found", "Tiny couldn't find this file");
+    //     return;
+    // }
 
-    sprintf(header, "HTTP/1.0 200 OK\r\n");
-    sprintf(header, "%sServer: Tiny Web Server\r\n", header);
-    sprintf(header, "%sConnection: close\r\n", header);
-    sprintf(header, "%sContent-length: %ld\r\n", header, sbuf.st_size);
-    sprintf(header, "%sContent-type: %s\r\n\r\n", header, "text/plain");
-    Rio_writen(fd, header, strlen(header));
+    // sprintf(header, "HTTP/1.0 200 OK\r\n");
+    // sprintf(header, "%sServer: Tiny Web Server\r\n", header);
+    // sprintf(header, "%sConnection: close\r\n", header);
+    // sprintf(header, "%sContent-length: %ld\r\n", header, sbuf.st_size);
+    // sprintf(header, "%sContent-type: %s\r\n\r\n", header, "text/plain");
+    // Rio_writen(fd, header, strlen(header));
 
-    srcp = Mmap(0, sbuf.st_size, PROT_READ, MAP_PRIVATE, ptr, 0);
-    Close(ptr);
-    Rio_writen(fd, srcp, sbuf.st_size);
+    // srcp = Mmap(0, sbuf.st_size, PROT_READ, MAP_PRIVATE, ptr, 0);
+    // Close(ptr);
+    // Rio_writen(fd, srcp, sbuf.st_size);
 
     return;
 }
@@ -224,6 +219,8 @@ void get_filetype(char *filename, char *filetype)
         strcpy(filetype, "image/png");
     else if(strstr(filename, ".jpg"))
         strcpy(filetype, "image/jpeg");
-    else   
+    else if(strstr(filename, ".mp4"))
+        strcpy(filetype, "video/mp4");
+    else
         strcpy(filetype, "text/plain");
 }
